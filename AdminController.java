a96c09e0689620f4a13c32a7d38e18c3b4c075a4
KrisTan2326 <128054270+KrisTan2326@users.noreply.github.com>
2025-10-07 21:06:57 +0530

    Update AdminController.java

--- AdminController.java
+++ AdminController.java
@@ -7,7 +7,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 @RestController
 @RequestMapping("/api/v1/admin")
 @Tag(name = "4. Admin Operations", description = "APIs for administrative tasks")
@@ -39,6 +38,7 @@ public class AdminController {
     }
 
 }
+
 
 
 
