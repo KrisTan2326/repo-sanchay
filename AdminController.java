e9817e541e5112ed376563a4bb19a58a8c5376e9
KrisTan2326 <128054270+KrisTan2326@users.noreply.github.com>
2025-10-07 20:44:14 +0530

    Update AdminController.java

--- AdminController.java
+++ AdminController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-
 import java.io.IOException;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -42,6 +41,7 @@ public class AdminController {
     }
 
 }
+
 
 
 
